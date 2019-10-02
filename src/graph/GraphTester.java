79bcf55733eb6fa8309b70fed6309555087dbf5d
Joe-Demp <dempseyjoseph96@yahoo.com>
2019-10-02 17:30:54 +0100

    Tested UndirectedGraph#degree

--- src/graph/GraphTester.java
+++ src/graph/GraphTester.java
@@ -8,6 +8,10 @@ public class GraphTester {
 		File f = new File("sample_ug.txt");
 		UndirectedGraph mygraph = UndirectedGraph.makeGraph(f);
 		System.out.println(mygraph);
+		
+		System.out.println("\nThe degree of vertex 1 is: " + UndirectedGraph.degree(mygraph, 1));
+		System.out.println("The degree of vertex 0 is: " + UndirectedGraph.degree(mygraph, 0));
+		System.out.println("The degree of vertex 3 is: " + UndirectedGraph.degree(mygraph, 3));		
 	}
 
 }
